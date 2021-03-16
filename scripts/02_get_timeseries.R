fab487de5993bff230d32574089ea0036e0ef108
albhasan <albhasan@gmail.com>
2021-03-16 13:12:19 +0000

    Check for already processed years.

--- scripts/02_get_timeseries.R
+++ scripts/02_get_timeseries.R
@@ -15,6 +15,12 @@ out_file <- "/home/alber-d005/Documents/samples_mapbiomas/data/samples/samples"
 stopifnot(dir.exists(csv_dir))
 
 for (my_year in 2017:2018) {
+    print(sprintf("%s Staring year: %s", Sys.time(), my_year))
+    my_out_file <- paste0(out_file, "_", my_year, ".rds")
+    if (file.exists(my_out_file)) {
+        warning(sprintf("Year %s was already processed. Skipping...", my_year))
+        next()
+    }
     lc8_cube <- sits_cube(type        = "BDC",
                           url         = "http://datacube-005.dpi.inpe.br:8010/stac/",
                           name        = "cerrado",
@@ -24,10 +30,9 @@ for (my_year in 2017:2018) {
                           collection  = "LC8_30_16D_STK-1",
                           start_date  = paste0(my_year, "-01-01"),
                           end_date    = paste0(my_year, "-12-31"))
-    print(sprintf("%s Staring year: %s", Sys.time(), my_year))
     samples <- do_get_ts(in_dir = paste0(csv_dir, "/", my_year), 
-                         out_file = paste0(out_file, "_", my_year, ".rds"),
+                         out_file = my_out_file,
                          cube = lc8_cube, 
-                         multicores = 40)
+                         multicores = 46)
     print(sprintf("%s Finished year: %s", Sys.time(), my_year))
 }
